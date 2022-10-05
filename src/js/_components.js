f2d5f66e26cd999d39c1072b4bfd692ca19135eb
Yakup <yakupjoraev@gmail.com>
2022-10-05 16:09:37 +0300

    added header and footer/ fixed styles

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import "./components/read-also-slider";
 import "./components/f-a-q-accardion";
-import "./components/policy-question"
+import "./components/policy-question";
+import "./components/topResize"
--- src/js/components/policy-question.js
+++ src/js/components/policy-question.js
@@ -1,6 +1,10 @@
 function policyQuestionSelect() {
   const questionSelect = document.querySelector('.policy-question__select')
 
+  if (!questionSelect) {
+    return null
+  }
+
   questionSelect.addEventListener('click', function () {
     questionSelect.classList.toggle('policy-question-active')
   })
