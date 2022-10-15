fc188e04b52fe3a14a06f511a74eb6e0b5f0f9ea
Yakup <yakupjoraev@gmail.com>
2022-10-15 13:58:48 +0300

    fix inputs and added new styles

--- src/js/components/input-clear.js
+++ src/js/components/input-clear.js
@@ -1,8 +1,20 @@
-// let inputs = document.querySelectorAll(".form-tell-story__input")
-// let clearBtn = document.querySelector(".clear-input")
+// Вместо класса можно использовать data-атрибуты: "[data-clear-button]"
+document.querySelectorAll(".clear-input")
+.forEach(function (elem) {
+  elem.onclick = function (e) {
+    // Если необходимо предотвратить/отменить событие по умолчанию,
+    // то необходимо вызвать метод preventDefault у события
+    // https://developer.mozilla.org/ru/docs/Web/API/Event/preventDefault
+    // e.preventDefault();
+    // если необходимо также предотвратить дальнейшее "всплытие" события,
+    // то необходимо вызвать метод stopPropagation у события
+    // https://developer.mozilla.org/ru/docs/Web/API/Event/stopPropagation
+    // e.stopPropagation();
+    let selector = this.dataset.clearSelector;
+    document.querySelectorAll(".form-tell-story__input")
+    .forEach(function (item) {
+      item.value = "";
+    });
+  };
+});
 
-// inputs.forEach(input => {
-//   if (input.value.length > 0) {
-//     clearBtn.style.display = 'block'
-//   }
-// });
