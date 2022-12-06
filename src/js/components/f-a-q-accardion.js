1ea000c9ee6cd069ba6a43b79bc9f964ba0207bf
Yakup <yakupjoraev@gmail.com>
2022-12-06 15:45:10 +0300

    other fixes

--- src/js/components/f-a-q-accardion.js
+++ src/js/components/f-a-q-accardion.js
@@ -1,18 +1,32 @@
 // Аккордеон
-function faqAccordion() {
+// function faqAccordion() {
+//   const items = document.querySelectorAll('.accordion-f-a-q__item-trigger')
+//   items.forEach(item => {
+//       item.addEventListener('click', () => {
+//           const parent = item.parentNode
+//           if (parent.classList.contains('accordion-f-a-q__item-active')) {
+//               parent.classList.remove('accordion-f-a-q__item-active')
+//           } else {
+//               document
+//                   .querySelectorAll('.accordion-f-a-q__item')
+//                   .forEach(child => child.classList.remove('accordion-f-a-q__item-active'))
+//               parent.classList.add('accordion-f-a-q__item-active')
+//           }
+//       })
+//   })
+// }
+// faqAccordion();
+
+function accardionTwo() {
   const items = document.querySelectorAll('.accordion-f-a-q__item-trigger')
+
   items.forEach(item => {
-      item.addEventListener('click', () => {
-          const parent = item.parentNode
-          if (parent.classList.contains('accordion-f-a-q__item-active')) {
-              parent.classList.remove('accordion-f-a-q__item-active')
-          } else {
-              document
-                  .querySelectorAll('.accordion-f-a-q__item')
-                  .forEach(child => child.classList.remove('accordion-f-a-q__item-active'))
-              parent.classList.add('accordion-f-a-q__item-active')
-          }
-      })
+    const parent = item.parentNode
+    parent.addEventListener('click', () => {
+      document.querySelectorAll('.accordion-f-a-q__item')
+      parent.classList.toggle('accordion-f-a-q__item-active')
+    })
   })
+  console.log(items);
 }
-faqAccordion()
+accardionTwo();
