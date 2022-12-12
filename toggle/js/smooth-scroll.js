1bb288e699078cf5734c6b50df2a66293cd84bbe
Yakup <yakupjoraev@gmail.com>
2022-12-12 10:01:19 +0300

    fix and redisign

--- toggle/js/smooth-scroll.js
+++
@@ -1,9 +0,0 @@
-var $page = $('html, body');
-$('.detal-instructions__links-link[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
-    return false;
-});
-
-
